472dbd2f5e1a0c9eee0d3aa6b91dacfd7894a83a
SmitCoderX <anirudh16june@gmail.com>
2024-06-19 23:16:15 +0530

    Fixed Requests

--- routes/request.js
+++ routes/request.js
@@ -10,10 +10,10 @@ const { protect, authorize } = require('../middleware/auth');
 
 router.route('/sendrequest').post(sendRequest);
 
-router.route('/:id').put(protect, requestStatus);
+router.route('/:id').put(requestStatus);
 
-router.route('/recipientRequests/:id').get(protect, getRecipientRequestes);
+router.route('/recipientRequests/:id').get(getRecipientRequestes);
 
-router.route('/requesterRequests/:id').get(protect, getRequesterRequestes);
+router.route('/requesterRequests/:id').get(getRequesterRequestes);
 
 module.exports = router;
